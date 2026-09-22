b95594bbaa471facb84891d85c811b68012273c6
ZaccBaker <zac.baker232@gmail.com>
2026-09-21 20:28:05 -0500

    init final

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import '../style/Home.css';
+
+
+function Home(){
+
+
+    return (
+        <div className="page home">
+            <p>Home Page</p>
+            
+        </div>
+    );
+}
+
+export default Home
